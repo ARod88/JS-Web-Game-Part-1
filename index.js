6bb6464ce6bc3922abb6678561c7ffef65fba933
ARod88 <alma.rodriguez72@yahoo.com>
2023-03-01 20:09:19 -0800

    invoke function green character

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 function newImage(url,left,bottom) {
+
+
 let greenCharacter = document.createElement('img')
 greenCharacter.src = 'assets/green-character.gif'
 greenCharacter.style.position = 'fixed'
@@ -12,9 +14,3 @@ document.body.append(greenCharacter)
 newImage('url,left,bottom')
 
 
-let pineTree = document.createElement('img')
-pineTree.src = 'assets/pine-tree.png'
-pineTree.style.position = 'fixed'
-pineTree.style.left = '450px'
-pineTree.style.bottom = '200px'
-document.body.append(pineTree)
