8e4525672a677ca20d6fb4fd35f4406379ac5a8b
ARod88 <alma.rodriguez72@yahoo.com>
2023-03-01 23:17:36 -0800

    added for loops

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
+let object = ['greenCharacter','pillar','crate','well']
+
+for (let index = 0; index < object.length;index++){
+    const element = object[index];
+}
+
+
+
 function newImage(url,left,bottom) {
 
 
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+    let object = document.createElement('img')
+    object.src = 'assets/green-character.gif'
+    object.style.position = 'fixed'
+    object.style.left = '100px'
+    object.style.bottom = '100px'
+    document.body.append(object)
+    
+  }
+    
+  newImage('$assets/tree.png,200,300')
+  newImage('assets/pillar.png,350px,100px')
+  newImage('assets/crate.png,150px,200px')
 
-
-}
-
-newImage('url,left,bottom')
-
-
+    
+    
